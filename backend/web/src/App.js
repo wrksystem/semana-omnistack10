39bff905c46907d69fb4ed3c6a4155aa078e7b31
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2021-07-20 23:12:35 -0300

    Estruturação  do App.js
    Estruturação inicial do App.js a partir de JSX, além da seleção e modificação dos arquivos

--- backend/web/src/App.js
+++ backend/web/src/App.js
@@ -3,12 +3,28 @@ import React from 'react';
 import './global.css';
 import './App.css';
 
-
 function App() {
   return (
     <div id='app'>
       <aside>
-      
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required /> 
+          </div>
+          
+          <div class="input-block">
+            <label htmlFor="tech">Tecnologias</label>
+            <input name="tech" id="tech" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="username_github">Usuário do GitHub</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+        </form>     
       </aside>
       <main>
 
